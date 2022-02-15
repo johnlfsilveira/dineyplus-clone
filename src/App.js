4474f989299594273381416ca5310904f1714cde
John L.F Silveira <99263330+johnlfsilveira@users.noreply.github.com>
2022-02-15 20:44:47 -0300

    Criaçao de component para Login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h2>Titulo</h2>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
